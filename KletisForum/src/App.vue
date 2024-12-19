c6ea2f9a2f69707af764eb5fcd4036a8a29648cd
Martynas Kuliešius <martiss200@gmail.com>
2024-12-19 16:46:13 +0200

    ADDED FOOTER

--- KletisForum/src/App.vue
+++ KletisForum/src/App.vue
@@ -14,11 +14,14 @@ import NotificationDisplay from '@/components/displays/NotificationDisplay.vue';
     <header class="bg-JDmidGreen h-auto pt-10 ">
       <NavBar/>
       <Modal></Modal>
-      <NotificationDisplay></NotificationDisplay>
     </header>
     <div class="pt-20 flex justify-center items-center h-full mx-auto" style="width:1600px">
+      <NotificationDisplay></NotificationDisplay>
       <RouterView />
     </div>
+    <footer>
+      Klėtis Forum © 2024
+    </footer>
   </main>
     
 </template>
